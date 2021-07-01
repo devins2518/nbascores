315bcc4254ac1a816ade2359859814d1ab3fa2b2
Devin Singh <drsingh2518@icloud.com>
2021-07-01 10:32:57 -0500

    better cli

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod boxscore;
 mod schedule;
 mod today_scoreboard;
 mod utils;
-use clap::{App, Arg, SubCommand};
+use clap::{App, AppSettings, Arg, SubCommand};
 use utils::PrettyPrintGame;
 
 static VERSION: &str = "0.1";
@@ -15,38 +15,47 @@ fn main() {
         .subcommand(
             SubCommand::with_name("get")
                 .about("print the most up to date data")
+                .setting(AppSettings::ArgRequiredElseHelp)
                 .arg(
                     Arg::with_name("game")
                         .conflicts_with("boxscore")
+                        .required_unless("boxscore")
                         .help("Print the most up to date information about a game"),
                 )
                 .arg(
                     Arg::with_name("boxscore")
-                        .conflicts_with("boxscore")
+                        .conflicts_with("game")
+                        .required_unless("game")
                         .help("Print the most up to date boxscore of a game"),
                 ),
         )
         .subcommand(
             SubCommand::with_name("watch")
                 .about("watch the most up to date game info as it updates")
+                .setting(AppSettings::ArgRequiredElseHelp)
                 .arg(
                     Arg::with_name("game")
                         .conflicts_with("boxscore")
+                        .required_unless("boxscore")
                         .help("Watch the game score"),
                 )
                 .arg(
                     Arg::with_name("boxscore")
-                        .conflicts_with("boxscore")
+                        .conflicts_with("game")
+                        .required_unless("game")
                         .help("Watch the boxscore"),
                 ),
         )
         .arg(
             Arg::with_name("date")
                 .short("d")
-                .default_value("today")
+                .takes_value(true)
+                .required(false)
                 .global(true)
-                .help("Date of the game in yyyy-mm-dd format"),
+                .help("Date of the game in yyyy-mm-dd format, defaults to today"),
         )
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .setting(AppSettings::ColoredHelp)
         .get_matches();
 
     let client = reqwest::blocking::Client::new();
@@ -55,9 +64,21 @@ fn main() {
         .value_of("date")
         .unwrap_or(&utils::today())
         .to_string();
-    let games = sc.get_date_game_id(date.clone());
-    games.iter().for_each(|x| {
-        let bs = boxscore::BoxScore::new(&client, date.clone(), &x).expect("Could not parse json!");
-        bs.print_game();
-    });
+
+    match matches.subcommand_name() {
+        Some("get") => {
+            if matches.is_present("boxscore") {
+                let games = sc.get_date_game_id(date.clone());
+                games.iter().for_each(|x| {
+                    let bs = boxscore::BoxScore::new(&client, date.clone(), &x)
+                        .expect("Could not parse json!");
+                    bs.print_game();
+                });
+            }
+        }
+        Some("watch") => {
+            println!("watch")
+        }
+        _ => (),
+    }
 }
